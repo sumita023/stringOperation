01c1e18b3796eeb29ea1103679c7a5d1cac78b91
Sumita Bhowmick <bhowmick.sumita18@gmail.com>
2023-07-17 11:03:28 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public class Program { 
+    public static void Main(string[] args)
+    {
+        TotalCountOfLetter("helloliza");
+    }
+    public static void TotalCountOfLetter(string str)
+    {
+        var query = str.GroupBy(x => x).Select(x => new
+        {
+            key=x.Key,
+            total=x.Count()
+        }).ToList();
+        int max = 0;
+        char newKey=char.MinValue;
+        foreach(var item in query)
+        {
+            if (item.total > max)
+            {
+                max = item.total;
+                newKey = item.key;
+            }
+        }
+
+        Console.WriteLine(newKey + " - " + max);
+        Console.ReadLine();
+    }
+}
+
